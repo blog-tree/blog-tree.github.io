b63f98e63faca6f3d096ba5831c3cafd9139f2fa
Oleksandr Zagovorychev <zagovorichev@gmail.com>
2021-10-27 14:18:46 +0200

    Updates

---
+++ precache-manifest.18f2e4016a414b393a407ab81144e628.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39fa09ec6c2c1576aecc",
+    "url": "/static/css/main.9ebaf8ae.chunk.css"
+  },
+  {
+    "revision": "39fa09ec6c2c1576aecc",
+    "url": "/static/js/main.84ff434f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "70b7c1a68354f30127df",
+    "url": "/static/js/2.58f87fe5.chunk.js"
+  },
+  {
+    "revision": "588e91b645764efa7461186b39d327b8",
+    "url": "/index.html"
+  }
+];
